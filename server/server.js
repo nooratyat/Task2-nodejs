bac03cca45112a701239603f2b972a755295e364
noor atyat <nooratyat92@gmail.com>
2017-05-30 17:34:02 +0300

    edit client side

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 var s3 = require('s3');
+
 require('./config/middleware.js')(app, express);
+
 app.use('/', express.static('../client'));
+
 app.get('/audio', function(req, res) {  
    var params = {
     Bucket: 'New-Bucket-1020',
@@ -10,16 +13,40 @@ app.get('/audio', function(req, res) {
   };
 })
 	
-	app.listen(8000 , function () {
-		console.log('...Server now listening on port ' +8000);
-	});
-
+	
 
 // var client = s3.createClient({  
 //   s3Options: {
-//     accessKeyId: <YOUR ACCESS KEY>,
-//     secretAccessKey: <YOUR SECRET KEY>
+//     accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+//     secretAccessKey:' wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 //   }
 // });
+
+
+
+// var downloadStream = client.downloadStream(params = {
+//     Bucket: 'New-Bucket-1020',
+//     Key: 'test.mp3'
+//   })
+
+  // downloadStream.on('error', function() {
+  //   res.status(404).send('Not Found');
+  // });
+  // downloadStream.on('httpHeaders', function(statusCode, headers, resp) {
+  //   // Set Headers
+  //   res.set({
+  //     'Content-Type': headers['content-type']
+  //   });
+  //     downloadStream.pipe(resp);
+
+  // });
+
+  // Pipe download stream to response
+
+
+
+app.listen(8000 , function () {
+		console.log('...Server now listening on port ' +8000);
+	});
 
 module.exports = app;
