eaab04db840efa4eae1a4d5db14223a949db0dfd
noor atyat <nooratyat92@gmail.com>
2017-05-30 14:28:18 +0300

    install s3

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 var express = require('express');
-
 var app = express();
-
+var s3 = require('s3');
 require('./config/middleware.js')(app, express);
+app.use('/', express.static('../client'));
+app.get('/audio', function(req, res) {  
+   var params = {
+    Bucket: 'New-Bucket-1020',
+    Key: 'test.mp3'
+  };
+})
 	
-	app.listen(3000 , function () {
-		console.log('...Server now listening on port ' +3000);
+	app.listen(8000 , function () {
+		console.log('...Server now listening on port ' +8000);
 	});
 
+
+// var client = s3.createClient({  
+//   s3Options: {
+//     accessKeyId: <YOUR ACCESS KEY>,
+//     secretAccessKey: <YOUR SECRET KEY>
+//   }
+// });
 
 module.exports = app;
