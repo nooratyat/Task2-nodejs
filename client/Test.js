33ef7f2b8ffac59dcf6d878875cf60902e21abea
noor atyat <nooratyat92@gmail.com>
2017-05-30 22:42:37 +0300

    make rout for upload and download

---
+++ client/Test.js
@@ -0,0 +1,78 @@
+// var express = require('express');
+// var app = express();
+// var s3 = require('s3');
+
+
+// require('./config/middleware.js')(app, express);
+
+// app.use(express.static(__dirname + '/../../client'));
+
+// app.get('/audio', function(req, res) {  
+//    var params = {
+//     Bucket: 't2',
+//     Key: 'test.mp3'
+//   };
+// })
+	
+	
+
+// var client = s3.createClient({  
+//   s3Options: {
+//     accessKeyId: 'AKIAICM7D3TQOUAI6SLQ',
+//     secretAccessKey:'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'
+//   }
+// });
+
+
+
+// exports.upload = function(req, res) {
+//     var file = req.files.file;
+//     var s3bucket = new AWS.S3({params: {Bucket: 't2'}});
+//     s3bucket.createBucket(function() {
+//         var params = {
+//             Key: file.name,
+//             Body: file
+//         };
+//         s3bucket.upload(params, function(err, data) {
+//             console.log("PRINT FILE:", file);
+//             if (err) {
+//                 console.log('ERROR MSG: ', err);
+//             } else {
+//                 console.log('Successfully uploaded data');
+//             }
+//         });
+//     });
+// };
+
+
+
+
+
+// // var downloadStream = client.downloadStream( params = {
+// //     Bucket: 't2',
+// //     Key: 'test.mp3'
+// //   })
+
+//   // downloadStream.on('error', function() {
+//   //   res.status(404).send('Not Found');
+//   // });
+//   // downloadStream.on('httpHeaders', function(statusCode, headers, resp) {
+//   //   // Set Headers
+//   //   res.set({
+//   //     'Content-Type': headers['content-type']
+//   //   });
+//   //     downloadStream.pipe(resp);
+
+//   // });
+
+//   // Pipe download stream to response
+
+
+
+// app.listen(8000 , function () {
+// 		console.log('...Server now listening on port ' +8000);
+// 	});
+
+// module.exports = app;
+
+
--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
-var s3 = require('s3');
+var router = express.Router();
+ var s3 = require('s3');
+var multer = require('multer');
+var upload = multer({
+  dest: "tmp/" 
+});
+var fs = require('fs');
+var AWS = require('aws-sdk');
 
+// Configure AWS SDK here
+// var s3 =  AWS.s3({
+//   params: {
+//     Bucket: 'xxx'
+//   }
+// });
 require('./config/middleware.js')(app, express);
-
 app.use(express.static(__dirname + '/../../client'));
-
-app.get('/audio', function(req, res) {  
-   var params = {
-    Bucket: 'New-Bucket-1020',
-    Key: 'test.mp3'
-  };
-})
-	
-	
 
 var client = s3.createClient({  
   s3Options: {
@@ -24,26 +27,39 @@ var client = s3.createClient({
 
 
 
-var downloadStream = client.downloadStream( params = {
-    Bucket: 'New-Bucket-1020',
-    Key: 'test.mp3'
-  })
+// Route for the upload
+app.post("/files/upload", upload.single("form-field-name"), function (req, res) {
+  var fileInfo = console.log(req.file);
+  var fileStream = fs.readFileSync(fileInfo.path);
+  var options = {
+    Bucket: 'xxx',
+    Key: 'yyy/'+fileName,
+    Body: fileStream
+  };
 
-  // downloadStream.on('error', function() {
-  //   res.status(404).send('Not Found');
-  // });
-  // downloadStream.on('httpHeaders', function(statusCode, headers, resp) {
-  //   // Set Headers
-  //   res.set({
-  //     'Content-Type': headers['content-type']
-  //   });
-  //     downloadStream.pipe(resp);
+  s3.upload(options, function (err) {
+    // Remove the temporary file
+    fs.removeFileSync("tmp/"+fileInfo.path); 
+    if (err) {
+      return res.status(500).end("Upload to s3 failed");
+    }
+    res.status(200).end("File uploaded");
+  });
+});
 
-  // });
-
-  // Pipe download stream to response
-
-
+// Route for the download
+app.get("/files/download/:name", function (req, res) {
+  var fileName = req.params.name;
+  if (!fileName) {
+    return res.status(400).end("missing file name");
+  }
+  var options = {
+    Bucket: 'xxx',
+    Key: 'yyy/'+fileName
+  };
+  res.attachment(fileName);
+  s3.getObject(options).createReadStream().pipe(res);
+});
 
 app.listen(8000 , function () {
 		console.log('...Server now listening on port ' +8000);
